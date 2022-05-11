ad86134c86ee809a6f2fedf0e9805fc3fee92664
Lin1ey <nguyen.numbers@gmail.com>
2022-05-10 20:55:44 -0400

    base code for website

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactMe from './pages/ContactMe';
+import Projects from './pages/projects/Projects';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+        <Route exact path="/contact-me">
+          <ContactMe />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,133 @@
+import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import Container from "@material-ui/core/Container";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import color from "@material-ui/core/colors";
+import Tooltip from "@material-ui/core/Tooltip";
+import MenuItem from "@material-ui/core/MenuItem";
+import AdbIcon from "@material-ui/icons/Adb";
+import { colors } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const pages = [
+  {
+    buttonName: "Contact Me",
+    path: "/contact-me",
+  },
+  {
+    buttonName: "Projects",
+    path: "/projects",
+  },
+  {
+    buttonName: "About",
+    path: "/about",
+  },
+  {
+    buttonName: "Home",
+    path: "/",
+  },
+];
+
+const useStyles = makeStyles({
+  button: {
+    color: "white",
+    fontSize: "1em",
+    // "&:hover": {
+    //   backgroundColor: "#545253",
+    // },
+  },
+  toolbar: {
+    display: "flex",
+    flexDirection: "row-reverse",
+  },
+});
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const classes = useStyles();
+
+  /*
+  {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                size="large"
+                variant="contained"
+                sx={{ my: 2, display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+            
+            
+<div>
+      <AppBar
+        position="static"
+        style={{ backgroundColor: colors.grey[900], minHeight: "70px" }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h4" style={{ flexGrow: 1 }}>
+              Brian Nguyen
+            </Typography>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                size="large"
+                onClick={handleCloseNavMenu}
+                style={{ color: "white", fontSize: "1.1em", mx: 10 }}
+                className={classes.button}
+              >
+                {page}
+              </Button>
+            ))}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+            
+            */
+
+  return (
+    <AppBar position="sticky" style={{ minHeight: "70px" }}>
+      <Toolbar className={classes.toolbar}>
+        {pages.map((page) => (
+          <Button
+            key={page.buttonName}
+            className={classes.button}
+            onClick={handleCloseNavMenu}
+            href={page.path}
+          >
+            {page.buttonName}
+          </Button>
+        ))}
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default Header;
