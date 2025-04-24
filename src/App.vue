2f6bee457637ce1456f1e9bc79bf45b6a6755491
Lin1ey <nguyen.numbers@gmail.com>
2025-04-24 19:23:03 -0400

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>HEADER + NAVBAR</div>
+  <div>ABOUT + LINKS</div>
+  <div>PROJECTS</div>
+  <div>FOOTER + LINKS</div>
+</template>
+
+<style scoped>
+
+</style>
