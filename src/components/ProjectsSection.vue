e440a7764606e255bf88f389d57a47044aba6020
Lin1ey <nguyen.numbers@gmail.com>
2025-05-03 21:55:31 -0400

    project section updated image source and added some css

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -3,13 +3,15 @@
         <div class="project-containers">
             <div class="project-card" v-for="(project, index) in projectsData" :key="index">
                 <div class="project-left">
-                    <img src='/images/bweeeh_amiya.jpg' />
+                    <img :src='getProjectImage(project.title, project.image)' />
                 </div>
                 <div class="project-right">
                     <h1>{{ project.title }}</h1>
                     <p>{{ project.desc }}</p>
                     <div class="project-buttons">
-                        BUTTON 1 BUTTON 2 BUTTON 3
+                        <button v-if="project.buttons.demo">DEMO</button>
+                        <button v-if="project.buttons.code">CODE</button>
+                        <button v-if="project.buttons.live">LIVE</button>
                     </div>
                 </div>
             </div>
@@ -27,7 +29,7 @@ import { ProjectData } from '@/types/projects';
 import DataService from '@/services/DataService';
 const dataService = new DataService()
 
-const getProjectImage = (title, image) => {
+const getProjectImage = (title: string, image: string) => {
     return `/images/${title}/${image}`
 }
 
@@ -41,6 +43,10 @@ onMounted(() => {
 </script>
 
 <style scoped>
+section {
+    padding: 50px 100px;
+}
+
 .project-containers {
     display: grid;
     grid-template-columns: 1fr 1fr;
@@ -50,17 +56,54 @@ onMounted(() => {
 .project-card {
     border: black solid 2px;
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--secondary);
 
+}
+
+.project-right {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    text-align: center;
+    padding: 8px;
+    line-height: 1.5rem;
+
+    h1 {
+        font-size: 2rem;
+    }
+
+    p {
+        font-size: 1rem;
+    }
 }
 
 img {
-    width: 150px;
-    height: 75px;
+    width: 250px;
+    height: 150px;
+}
+
+@media only screen and (min-width: 1300px) {
+    img {
+        width: 450px;
+        height: 250px;
+    }
 }
 
 @media only screen and (max-width: 768px) {
+    section {
+        height: 1000px;
+        padding: 50px 18px;
+    }
+
     .project-containers {
         grid-template-columns: 1fr;
+    }
+
+    img {
+        width: 280px;
+        height: 175px;
     }
 }
 </style>
